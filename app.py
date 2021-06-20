2985f870c5231dabcf8188a7c987ad11777c26f1
Elena Spirkina <sltolerantnaya@gmail.com>
2021-06-20 12:53:58 -0700

    ghanges to python code and account SID

--- app.py
+++ app.py
@@ -9,21 +9,37 @@ from flask import (
     request,
     url_for,
 )
+from twilio.rest import Client
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = "ssssh don't tell anyone"
 
+
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+
+client = Client()
 
 def get_sent_messages():
     # TODO: Make this return a collection of messages that were sent from the number
-    messages = []
-    return messages
+    messages = client.messages.list(from_=TWILIO_PHONE_NUMBER) # or limit=<some number, to limit number of msgs visible>
+
+#     for record in messages:      this line of code print out list of all msgs in the top of complimenter app UI, which is visible from external surses as well. 
+#         print(record.sid)
+    # return messages
+    # messages = []
+    # return messages
 
 def send_message(to, body):
     # TODO: Send the text message
-    pass
+    
+     client.messages.create(
+       to=to,
+       body=body,
+       from_=TWILIO_PHONE_NUMBER
+       
+    )
+
 
 @app.route("/", methods=["GET"])
 def index():
